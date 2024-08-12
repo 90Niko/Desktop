7b3e380ba152a482ef1777a5881e562032a13a5c
90Niko <110725784+90Niko@users.noreply.github.com>
2024-08-12 11:46:56 +0200

    Update

--- Lecture-15/09.HTTP-and-AJAX-Exercise-Resources/04.Students/app.js
+++ Lecture-15/09.HTTP-and-AJAX-Exercise-Resources/04.Students/app.js
@@ -1,5 +1,64 @@
 function attachEvents() {
-  // TODO:
+  const baseUrl = "http://localhost:3030/jsonstore/collections/students";
+
+  const tableInfo = document.querySelector('table tbody');
+  const [firstNameEl, lastNameEl, facultnumberEl, gradeEl] = document.querySelectorAll("input[type=text]");
+
+  const btnSubmit = document.getElementById('submit');
+
+  
+
+  btnSubmit.addEventListener('click', () => {
+
+    fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'aplicationjson'
+      },
+      body: JSON.stringify({
+
+        firstName: firstNameEl.value,
+        lastName: lastNameEl.value,
+        facultyNumber: facultnumberEl.value,
+        grade: gradeEl.value
+
+      })
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
+
+    firstNameEl.value = "";
+    lastNameEl.value = "";
+    facultnumberEl.value = "";
+    gradeEl.value = "";
+
+  });
+  
+  fetch(baseUrl)
+    .then(res => res.json())
+    .then(result => {
+      const allStudents = Object.values(result);
+      for (const student of allStudents) {
+
+
+       const thFirstName=document.createElement('th');
+       thFirstName.textContent = student.firstName;
+      
+       const thLastName=document.createElement('th');
+       thLastName.textContent = student.lastName;
+       
+        const thfacultNUmberName=document.createElement('th');
+        thfacultNUmberName.textContent = student.facultyNumber;
+       
+        const thGrade=document.createElement('th');
+        thGrade.textContent = student.grade;
+       
+        const trEl = document.createElement('tr');
+        trEl.append(thFirstName,thLastName,thfacultNUmberName,thGrade);
+
+        tableInfo.appendChild(trEl);
+      }
+    });
 }
 
 attachEvents();
